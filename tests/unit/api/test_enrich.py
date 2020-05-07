9fee607ffd4b9c008a5cd3d916a6c9fb2fd5f2c0
GeorgiyII <gosha1104@gmail.com>
2020-05-07 17:39:22 +0300

    added unittests

--- tests/unit/api/test_enrich.py
+++ tests/unit/api/test_enrich.py
@@ -1,12 +1,20 @@
 from pytest import fixture
+from unittest import mock
+from http import HTTPStatus
 
 from .utils import headers
+from tests.unit.mock_for_tests import (
+    BREACH_EMAIL_RESPONSE_MOCK,
+    EXPECTED_SUCCESS_RESPONSE,
+    EXPECTED_RESPONSE_401_ERROR,
+    EXPECTED_RESPONSE_403_ERROR,
+    EXPECTED_RESPONSE_500_ERROR,
+    EXPECTED_RESPONSE_404_ERROR
+)
 
 
 def routes():
-    yield '/deliberate/observables'
     yield '/observe/observables'
-    yield '/refer/observables'
 
 
 @fixture(scope='module', params=routes(), ids=lambda route: f'POST {route}')
@@ -14,9 +22,31 @@ def route(request):
     return request.param
 
 
-def test_enrich_call_with_invalid_jwt_failure(route, client, invalid_jwt):
-    response = client.post(route, headers=headers(invalid_jwt))
-    assert response
+@fixture(scope='function')
+def spycloud_api_request():
+    with mock.patch('requests.get') as mock_request:
+        yield mock_request
+
+
+def spycloud_api_response(*, ok, status_error=None, payload=None):
+    mock_response = mock.MagicMock()
+
+    if payload:
+        payload = payload
+
+    mock_response.ok = ok
+
+    if ok:
+        if not payload:
+            payload = BREACH_EMAIL_RESPONSE_MOCK
+
+    else:
+        mock_response.status_code = status_error
+        mock_response.text = str(payload)
+
+    mock_response.json = lambda: payload
+
+    return mock_response
 
 
 @fixture(scope='module')
@@ -24,21 +54,89 @@ def invalid_json():
     return [{'type': 'unknown', 'value': ''}]
 
 
-def test_enrich_call_with_valid_jwt_but_invalid_json_failure(route,
-                                                             client,
-                                                             valid_jwt,
-                                                             invalid_json):
-    response = client.post(route,
-                           headers=headers(valid_jwt),
-                           json=invalid_json)
-    assert response
+def test_enrich_call_with_invalid_json_failure(route, client, valid_jwt,
+                                               invalid_json):
+    response = client.post(
+        route, headers=headers(valid_jwt), json=invalid_json
+    )
+
+    expected_payload = {
+        'errors': [
+            {
+                'code': 'invalid argument',
+                'message': mock.ANY,
+                'type': 'fatal',
+            }
+        ]
+    }
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == expected_payload
 
 
 @fixture(scope='module')
 def valid_json():
-    return [{'type': 'domain', 'value': 'cisco.com'}]
+    return [{'type': 'email', 'value': 'admin@example.org'}]
 
 
-def test_enrich_call_success(route, client, valid_jwt, valid_json):
-    response = client.post(route, headers=headers(valid_jwt), json=valid_json)
-    assert response
+def test_enrich_call_success(route, client, valid_jwt, valid_json,
+                             spycloud_api_request):
+
+    spycloud_api_request.return_value = spycloud_api_response(ok=True)
+
+    response = client.post(
+        route, headers=headers(valid_jwt), json=valid_json
+    )
+
+    assert response.status_code == HTTPStatus.OK
+
+    data = response.get_json()
+
+    assert data['data']['sightings']['docs'][0].pop('id')
+    assert data['data']['sightings']['docs'][1].pop('id')
+
+    assert data == EXPECTED_SUCCESS_RESPONSE
+
+
+def test_enrich_call_auth_error(route, client, valid_jwt, valid_json,
+                                spycloud_api_request):
+    spycloud_api_request.return_value = spycloud_api_response(ok=False,
+                                                              status_error=401)
+    response = client.post(
+        route, headers=headers(valid_jwt),  json=valid_json
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_401_ERROR
+
+
+def test_enrich_call_permission_error(route, client, valid_jwt, valid_json,
+                                      spycloud_api_request):
+    spycloud_api_request.return_value = spycloud_api_response(ok=False,
+                                                              status_error=403)
+    response = client.post(
+        route, headers=headers(valid_jwt), json=valid_json
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_403_ERROR
+
+
+def test_enrich_call_404(route, client, valid_jwt, valid_json,
+                         spycloud_api_request):
+    spycloud_api_request.return_value = spycloud_api_response(ok=False,
+                                                              status_error=404)
+    response = client.post(
+        route, headers=headers(valid_jwt), json=valid_json
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_404_ERROR
+
+
+def test_enrich_call_500(route, client, valid_jwt, valid_json,
+                         spycloud_api_request):
+    spycloud_api_request.return_value = spycloud_api_response(ok=False,
+                                                              status_error=500)
+    response = client.post(
+        route, headers=headers(valid_jwt), json=valid_json
+    )
+    assert response.status_code == HTTPStatus.OK
+    assert response.get_json() == EXPECTED_RESPONSE_500_ERROR
--- tests/unit/mock_for_tests.py
+++ tests/unit/mock_for_tests.py
@@ -17,6 +17,55 @@ WATCHLIST_RESPONSE_MOCK = {
     ]
 }
 
+BREACH_EMAIL_RESPONSE_MOCK = {
+    "cursor": "",
+    "hits": 2,
+    "results": [
+        {
+            "city": "()",
+            "domain": "example.org",
+            "infected_path": "C:/Users/Usama/AppData/Local/Temp/6210230526.exe",
+            "infected_time": "2020-03-26 16:56:49",
+            "country": "()",
+            "isp": "()",
+            "email_username": "admin",
+            "email": "admin@example.org",
+            "spycloud_publish_date": "2020-04-16T00:00:00Z",
+            "password": "test123",
+            "target_url": "http://localhost:3000/en/session",
+            "sighting": 1,
+            "email_domain": "example.org",
+            "source_id": 17551,
+            "infected_machine_id": "0bb046a5-2ef2-4ec3-abbe-ba76f5b78e6c",
+            "password_plaintext": "test123",
+            "password_type": "plaintext",
+            "document_id": "90f9c47d-c86d-400f-9a57-38fed22b5fad",
+            "severity": 25
+        },
+        {
+            "domain": "example.org",
+            "target_url": "13.127.174.82",
+            "infected_machine_id": "2fc6eba4-738d-11ea-807f-0a7889d429db",
+            "user_browser": "Google Chrome New",
+            "country": "India",
+            "ip_addresses": [
+                "210.56.127.202"
+            ],
+            "email_username": "admin",
+            "email": "admin@example.org",
+            "spycloud_publish_date": "2020-04-02T00:00:00Z",
+            "password": "Admin@1234",
+            "sighting": 6,
+            "email_domain": "example.org",
+            "source_id": 17494,
+            "password_plaintext": "Admin@1234",
+            "password_type": "plaintext",
+            "document_id": "d27e8c1e-dd07-4237-bf51-40bcb5744fcc",
+            "severity": 25
+        }
+    ]
+}
+
 
 EXPECTED_RESPONSE_401_ERROR = {
     'errors': [
@@ -57,4 +106,131 @@ EXPECTED_RESPONSE_500_ERROR = {
             'type': 'fatal'
         }
     ]
+}
+
+
+EXPECTED_SUCCESS_RESPONSE = {
+    'data': {
+        'sightings': {
+            'count': 2,
+            'docs': [
+                {
+                    'confidence': 'Medium',
+                    'count': 1,
+                    'external_ids': [
+                        '90f9c47d-c86d-400f-9a57-38fed22b5fad',
+                        '17551',
+                        '0bb046a5-2ef2-4ec3-abbe-ba76f5b78e6c'
+                    ],
+                    'internal': False,
+                    'observables': [
+                        {
+                            'type': 'email',
+                            'value': 'admin@example.org'
+                        }
+                    ],
+                    'observed_time': {
+                        'start_time': '2020-04-16T00:00:00.000000Z'
+                    },
+                    'relations': [
+                        {
+                            'origin': 'Spycloud Breach Module',
+                            'related': {
+                                'type': 'email',
+                                'value': 'admin@example.org'
+                            },
+                            'relation': 'Leaked_From',
+                            'source': {
+                                'type': 'domain',
+                                'value': 'example.org'
+                            }
+                        }
+                    ],
+                    'schema_version': '1.0.16',
+                    'severity': 'High',
+                    'source': 'Spycloud',
+                    'targets': [
+                        {
+                            'observables': [
+                                {
+                                    'type': 'url',
+                                    'value': 'http://localhost:3000/en/session'
+                                }
+                            ],
+                            'observed_time': {
+                                'start_time': '2020-04-16T00:00:00.000000Z'
+                            },
+                            'type': 'email'
+                        }
+                    ],
+                    'title': 'Reported to Spycloud',
+                    'type': 'sighting'
+                },
+                {
+                    'confidence': 'Medium',
+                    'count': 6,
+                    'external_ids': [
+                        'd27e8c1e-dd07-4237-bf51-40bcb5744fcc',
+                        '17494',
+                        '2fc6eba4-738d-11ea-807f-0a7889d429db'
+                    ],
+                    'internal': False,
+                    'observables': [
+                        {
+                            'type': 'email',
+                            'value': 'admin@example.org'
+                        }
+                    ],
+                    'observed_time': {
+                        'start_time': '2020-04-02T00:00:00.000000Z'
+                    },
+                    'relations': [
+                        {
+                            'origin': 'Spycloud Breach Module',
+                            'related': {
+                                'type': 'email',
+                                'value': 'admin@example.org'
+                            },
+                            'relation': 'Leaked_From',
+                            'source': {
+                                'type': 'domain',
+                                'value': 'example.org'
+                            }
+                        },
+                        {
+                            'origin': 'Spycloud Breach Module',
+                            'related': {
+                                'type': 'email',
+                                'value': 'admin@example.org'
+                            },
+                            'relation': 'Leaked_From',
+                            'source': {
+                                'type': 'ip',
+                                'value': '210.56.127.202'
+                            }
+                        }
+                    ],
+                    'schema_version': '1.0.16',
+                    'severity': 'High',
+                    'source': 'Spycloud',
+                    'targets': [
+                        {
+                            'observables': [
+                                {
+                                    'type': 'ip',
+                                    'value': '13.127.174.82'
+                                }
+                            ],
+                            'observed_time': {
+                                'start_time': '2020-04-02T00:00:00.000000Z'
+                            },
+                            'type': 'email'
+                        }
+                    ],
+                    'title': 'Reported to Spycloud',
+                    'type': 'sighting'
+                }
+            ]
+        }
+    }
 }
