9f79a64d49b0dd2a5b9aa3576813334de64641c4
YevhenLysen <ylysen@softserveinc.com>
2020-09-29 17:28:22 +0300

    Add new test for empty observable

---
+++ tests/functional/tests/test_empty_observebles.py
@@ -0,0 +1,33 @@
+from ctrlibrary.core.utils import get_observables
+from ctrlibrary.threatresponse.enrich import enrich_observe_observables
+from tests.functional.tests.constants import MODULE_NAME
+
+
+def test_positive_smoke_empty_observable(module_headers):
+    """Perform testing for enrich observe observables endpoint to
+    check observables is returned empty from Spycloud module
+
+
+    ID: CCTRI-1695-2b2f141b-d2ac-4a26-a254-2f9524e5ad75
+
+    Steps:
+        1. Send request to enrich observe observable endpoint
+
+    Expectedresults:
+        1.Response body contains empty data dict from Spycloud module
+
+    Importance: Critical
+    """
+    observable = [{'type': 'email', 'value': 'marita67@example.org'}]
+    response_from_all_modules = enrich_observe_observables(
+        payload=observable,
+        **{'headers': module_headers}
+    )
+    response_from_spycloud_module = get_observables(
+        response_from_all_modules['data'], MODULE_NAME)
+
+    assert response_from_spycloud_module['module'] == MODULE_NAME
+    assert response_from_spycloud_module['module_instance_id']
+    assert response_from_spycloud_module['module_type_id']
+
+    assert response_from_spycloud_module['data'] == {}
