1af9f92fc70a255c0742faf1a39ecf628ada3dd8
YevhenLysen <ylysen@softserveinc.com>
2020-10-09 16:41:49 +0300

    Fix autotests

--- tests/functional/tests/constants.py
+++ tests/functional/tests/constants.py
@@ -1,4 +1,5 @@
-MODULE_NAME = SOURCE = 'SpyCloud'
+MODULE_NAME = 'SpyCloud'
 SPYCLOUD_URL = 'https://portal.spycloud.com/breach/catalog/'
 CONFIDENCE = SEVERITY = ('High', 'Medium', 'Low')
 CTR_ENTITIES_LIMIT = 100
+SOURCE = 'Spycloud'
