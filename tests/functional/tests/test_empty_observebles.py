4d990a298aa20626b24a57f626a87ea0bded55f4
YevhenLysen <ylysen@softserveinc.com>
2020-10-30 10:54:51 +0200

    Fix autotests

--- tests/functional/tests/test_empty_observebles.py
+++ tests/functional/tests/test_empty_observebles.py
@@ -24,9 +24,8 @@ def test_positive_smoke_empty_observables(module_headers):
         **{'headers': module_headers}
     )
 
-    spycloud_data = response_from_all_modules['data']
-
-    response_from_spycloud_module = get_observables(spycloud_data, MODULE_NAME)
+    response_from_spycloud_module = get_observables(response_from_all_modules,
+                                                    MODULE_NAME)
 
     assert response_from_spycloud_module['module'] == MODULE_NAME
     assert response_from_spycloud_module['module_instance_id']
